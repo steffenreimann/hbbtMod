6d2964d531a7126c7c2ee128ff0753a49fc3feac
Steffen <steffenreimann2011@googlemail.com>
2021-03-27 06:45:05 +0100

    Neus Building Tafel zum anzeigen von Text

---
+++ Source/FactoryGame/Public/Hologram/hbbtTafelHologram.h
@@ -0,0 +1,17 @@
+// ILikeBanas
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Hologram/FGBuildableHologram.h"
+#include "hbbtTafelHologram.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FACTORYGAME_API AhbbtTafelHologram : public AFGBuildableHologram
+{
+	GENERATED_BODY()
+	
+};
